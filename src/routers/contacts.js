e0cbf35b99309ce6d8c5dfd4b4ae5d905ba7d770
AlbinaKost <143324469+AlbinaKost@users.noreply.github.com>
2024-05-30 14:14:17 +0300

    upd'

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,9 +1,19 @@
-function ctrlWrapper(ctrl) {
-    return async (req, res, next) => {
-        try {
-            await ctrl(req, res, next);
-        } catch (error) {
-            next(error);
-        }
-    };
-}
+import { Router } from 'express';
+import {
+  createContactController,
+  deleteContactController,
+  getAllContactsController,
+  getContactByIdController,
+  patchContactController,
+} from '../controllers/contacts.js';
+
+import { ctrlWraper } from '../utils/ctrlWrapper.js';
+const router = Router();
+
+router.get('/', ctrlWraper(getAllContactsController));
+router.get('/:contactId', ctrlWraper(getContactByIdController));
+router.post('/', ctrlWraper(createContactController));
+router.delete('/:contactId', ctrlWraper(deleteContactController));
+router.patch('/:contactId', ctrlWraper(patchContactController));
+
+export default router;
