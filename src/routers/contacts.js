ac67001ba95a450e7ce6ea13f69675bcfe5b35e2
AlbinaKost <143324469+AlbinaKost@users.noreply.github.com>
2024-05-30 13:00:52 +0300

    upd

---
+++ src/routers/contacts.js
@@ -0,0 +1,9 @@
+function ctrlWrapper(ctrl) {
+    return async (req, res, next) => {
+        try {
+            await ctrl(req, res, next);
+        } catch (error) {
+            next(error);
+        }
+    };
+}
