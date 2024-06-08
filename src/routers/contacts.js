d154a86bdef01e99502a1a5e66f6341bc82f782e
AlbinaKost <143324469+AlbinaKost@users.noreply.github.com>
2024-06-08 20:49:09 +0300

    update

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -9,13 +9,13 @@ import {
 
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middleware/validateBody.js';
-import { createContactSchema } from '../validation/contacts.js';
+import { createContactSchema, updateContactSchema } from '../validation/contacts.js';
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getAllContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
 router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
-router.patch('/contacts/:contactId', validateBody(createContactSchema), ctrlWrapper(patchContactController));
+router.patch('/contacts/:contactId', validateBody(updateContactSchema), ctrlWrapper(patchContactController));
 
 export default router;
--- src/utils/parseSortParams.js
+++ src/utils/parseSortParams.js
@@ -2,7 +2,7 @@ import { SORT_ORDER } from '../constants/index.js';
 
 const parseSortOrder = (sortOrder) => {
   const isKnownOrder = [SORT_ORDER.ASC, SORT_ORDER.DESC].includes(sortOrder);
-  if (!isKnownOrder) {
+  if (!isKnownOrder) return sortOrder; {
     return SORT_ORDER.ASC;
   }
 };
