d41b16cab750b1a7628418f6d75b05e8b859235c
AlbinaKost <143324469+AlbinaKost@users.noreply.github.com>
2024-06-09 20:00:59 +0300

    Update ParseSortParams

--- src/utils/parseSortParams.js
+++ src/utils/parseSortParams.js
@@ -2,7 +2,7 @@ import { SORT_ORDER } from '../constants/index.js';
 
 const parseSortOrder = (sortOrder) => {
   const isKnownOrder = [SORT_ORDER.ASC, SORT_ORDER.DESC].includes(sortOrder);
-  if (!isKnownOrder) return sortOrder; {
+  if (isKnownOrder) return sortOrder; {
     return SORT_ORDER.ASC;
   }
 };
@@ -16,7 +16,7 @@ const parseSortBy = (sortBy) => {
     'isFavourite',
   ];
 
-  if (!keysOfContact.includes(sortBy)) {
+  if (keysOfContact.includes(sortBy)) {
     return sortBy;
   }
   return '_id';
