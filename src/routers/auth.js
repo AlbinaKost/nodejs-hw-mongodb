52c116365312f00c50efd58bea81ddb6d28e2216
AlbinaKost <143324469+AlbinaKost@users.noreply.github.com>
2024-06-15 15:55:34 +0300

    upd

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -6,9 +6,11 @@ import { validateBody } from '../middleware/validateBody.js';
 import { loginContactController } from '../controllers/auth.js';
 import { loginUserSchema } from '../validation/auth.js';
 import { logoutUserController } from '../controllers/auth.js';
+import { refreshUserSessionController } from '../controllers/auth.js';
 
 const router = Router();
 router.post('/register', validateBody(registerUserSchema), ctrlWrapper(registerContactController));
 router.post('/login', validateBody(loginUserSchema), ctrlWrapper(loginContactController));
 router.post('/logout', ctrlWrapper(logoutUserController));
+router.post('/refresh', ctrlWrapper(refreshUserSessionController));
 export default router;
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -10,12 +10,14 @@ import {
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middleware/validateBody.js';
 import { createContactSchema, updateContactSchema } from '../validation/contacts.js';
+import { authenticate } from '../middleware/authenticate.js';
 const router = Router();
-
+router.use(authenticate);
 router.get('/', ctrlWrapper(getAllContactsController));
 router.get('/:contactId', ctrlWrapper(getContactByIdController));
 router.post('', validateBody(createContactSchema), ctrlWrapper(createContactController));
 router.delete('/:contactId', ctrlWrapper(deleteContactController));
 router.patch('/:contactId', validateBody(updateContactSchema), ctrlWrapper(patchContactController));
+
 
 export default router;
