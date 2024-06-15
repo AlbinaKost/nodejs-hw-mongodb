fbc9c66cc75788c1e573b504b1072ba99fbd58e0
AlbinaKost <143324469+AlbinaKost@users.noreply.github.com>
2024-06-15 15:13:41 +0300

    upsd

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -3,8 +3,12 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { registerUserSchema } from '../validation/auth.js';
 import { registerContactController } from '../controllers/auth.js';
 import { validateBody } from '../middleware/validateBody.js';
-
+import { loginContactController } from '../controllers/auth.js';
+import { loginUserSchema } from '../validation/auth.js';
+import { logoutUserController } from '../controllers/auth.js';
 
 const router = Router();
 router.post('/register', validateBody(registerUserSchema), ctrlWrapper(registerContactController));
+router.post('/login', validateBody(loginUserSchema), ctrlWrapper(loginContactController));
+router.post('/logout', ctrlWrapper(logoutUserController));
 export default router;
