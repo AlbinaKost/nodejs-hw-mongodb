47e8c6796f064ef7ce7c8db557ea56ed7cb8759d
AlbinaKost <143324469+AlbinaKost@users.noreply.github.com>
2024-06-21 22:40:08 +0300

    upd

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,10 @@
+import path from 'node:path';
+
 export const SORT_ORDER = {
     ASC: 'asc',
     DESC: 'desc',
   };
+
   export const FIFTEEN_MINUTES = 15 * 60 * 1000;
   export const ONE_DAY = 24 * 60 * 60 * 1000;
 
@@ -11,4 +14,7 @@ export const SORT_ORDER = {
     SMTP_USER: 'SMTP_USER',
     SMTP_PASSWORD: 'SMTP_PASSWORD',
     SMTP_FROM: 'SMTP_FROM',
+    JWT_SECRET: 'JWT_SECRET',
   };
+
+  export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
